1b12b108b7ab6f7dd37820a2a22cc2b23dd59b20
Jaisinghal13 <60787800+Jaisinghal13@users.noreply.github.com>
2021-06-06 16:45:31 +0530

    Add files via upload

---
+++ Node.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Node() {
+  return (
+    <div className="note">
+      <h1>JavaScript and React.js</h1>
+      <p>
+        This is an amazing bootcamp by Shaurya Sinha Sir and in this bootcamp
+        only basics topic i.e.react.js has taught by sir. In this bootcamp you
+        will get certified with google in association with microsoft.
+      </p>
+    </div>
+    
+  );
+}
+export default Node;
